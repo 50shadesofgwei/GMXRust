6751ac287bd270f93b5ed5aed276d471ac563897
50 <jonofeasby4@gmail.com>
2023-11-16 15:44:17 +0000

    add anvil

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ pub mod contract_caller;
 
 use contract_caller::connect_provider::connect_provider;
 use dotenv::dotenv;
+use ethers::{utils::{Anvil, self}, signers::LocalWallet};
 
 #[tokio::main]
 async fn main() {
@@ -10,4 +11,9 @@ async fn main() {
         Ok(_) => println!("Successfully connected to the provider."),
         Err(e) => println!("Error connecting to the provider: {}", e),
     }
+
+    let anvil: utils::AnvilInstance = Anvil::new().spawn();
+    let wallet: LocalWallet = anvil.keys()[0].clone().into();
+
+    
 }
