68f518387e9253daaa8dec5021f98f597d996da7
50 <jonofeasby4@gmail.com>
2023-11-16 00:06:52 +0000

    WIP: commit progress

--- src/contract_caller/contract_caller.rs
+++ src/contract_caller/contract_caller.rs
@@ -1,2 +1,17 @@
 use alloy_json_abi::JsonAbi;
 
+fn get_abi() {
+    let path = "GMX_Rust/src/contract_caller/abis/exchange_router_abi.json";
+
+    match fs::read_to_string(path) {
+        Ok(json) => {
+            match serde_json::from_str::<JsonAbi>(&json) {
+                Ok(abi) => {
+                    // TODO
+                },
+                Err(e) => eprintln!("Failed to parse JSON: {}", e),
+            }
+        },
+        Err(e) => eprintln!("Failed to read file: {}", e),
+    }
+}
