301a45cf5858346c2b43cdb33d3b87f35d3bc0a7
50 <jonofeasby4@gmail.com>
2023-11-18 15:37:17 +0000

    init contracts

--- src/contract_caller/sol_call.rs
+++ src/contract_caller/sol_call.rs
@@ -4,20 +4,30 @@ use dotenv::dotenv;
 use std::env;
 use std::sync::Arc;
 
-abigen!{IERC721, "/Users/jfeasby/GMX Rust/GMX_Rust/src/contract_caller/abis/exchange_router_abi.json"}
+// Create contract instances w/ abigen
+abigen!{ 
+    EXCHANGE_ROUTER, "/Users/jfeasby/GMX Rust/GMX_Rust/src/contract_caller/abis/exchange_router_abi.json";
+    USDC_NATIVE, "/Users/jfeasby/GMX Rust/GMX_Rust/src/contract_caller/abis/usdc_arb_native_abi.json"
+}
 
 pub async fn sol_call() -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok();
 
     // Directly unwrap the address parsing result
-    let contract_address_str: String = "0x7C68C7866A64FA2160F78EEaE12217FFbf871fa8".to_string();
-    let contract_address: H160 = contract_address_str.parse()?;
+    let exchange_router_address_str: String = "0x7C68C7866A64FA2160F78EEaE12217FFbf871fa8".to_string();
+    let exchange_router_address: H160 = exchange_router_address_str.parse()?;
 
+    let usdc_native_address_str: String = "0xaf88d065e77c8cC2239327C5EDb3A432268e5831".to_string();
+    let usdc_native_address: H160 = usdc_native_address_str.parse()?;
+
+    // Initialise providers
     let rpc_url: String = env::var("PROVIDER_URL")?;
-
     let provider: Provider<Http> = Provider::<Http>::try_from(rpc_url.as_str())?;
     let provider: Arc<Provider<Http>> = Arc::new(provider);  // Wrap the provider in an Arc
-    let contract: IERC721<_> = IERC721::new(contract_address, provider.clone());
+
+    // Create contract instances
+    let exchange_router_contract: EXCHANGE_ROUTER<_> = EXCHANGE_ROUTER::new(exchange_router_address, provider.clone());
+    let usdc_native_contract: USDC_NATIVE<_> = USDC_NATIVE::new(usdc_native_address, provider.clone());
 
     let function_name: &str = "multicall";
     let function_params = ();
