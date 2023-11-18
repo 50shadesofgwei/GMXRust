d51cd117635231d6bc7017be520c1a32474d77dd
50 <jonofeasby4@gmail.com>
2023-11-18 16:42:33 +0000

    WIP: multicall builder

--- src/contract_caller/sol_call.rs
+++ src/contract_caller/sol_call.rs
@@ -4,17 +4,22 @@ use dotenv::dotenv;
 use std::env;
 use std::sync::Arc;
 
-// Create contract instances w/ abigen
+// Create contract instances from abis w/ abigen
 abigen!{ 
     EXCHANGE_ROUTER, "/Users/jfeasby/GMX Rust/GMX_Rust/src/contract_caller/abis/exchange_router_abi.json";
     USDC_NATIVE, "/Users/jfeasby/GMX Rust/GMX_Rust/src/contract_caller/abis/usdc_arb_native_abi.json";
     VAULT, "/Users/jfeasby/GMX Rust/GMX_Rust/src/contract_caller/abis/vault_abi.json";
+    WETH, "/Users/jfeasby/GMX Rust/GMX_Rust/src/contract_caller/abis/weth_abi.json";
 }
 
 pub async fn sol_call() -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok();
 
-    // Necessary preamble for contract instances
+    // ---------------------------------------------------------
+    //                      Initialisation
+    // ---------------------------------------------------------
+
+    // Some necessary preamble for contract instances
     // Exchange Router
     let exchange_router_address_str: String = "0x7C68C7866A64FA2160F78EEaE12217FFbf871fa8".to_string();
     let exchange_router_address: H160 = exchange_router_address_str.parse()?;
@@ -27,28 +32,42 @@ pub async fn sol_call() -> Result<(), Box<dyn std::error::Error>> {
     let vault_address_str: String = "0x489ee077994B6658eAfA855C308275EAd8097C4A".to_string();
     let vault_address: H160 = vault_address_str.parse()?;
 
+    // WETH (WNT)
+    let weth_address_str: String = "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1".to_string();
+    let weth_address: H160 = weth_address_str.parse()?;
+
 
     // Initialise providers
     let rpc_url: String = env::var("PROVIDER_URL")?;
     let provider: Provider<Http> = Provider::<Http>::try_from(rpc_url.as_str())?;
     let provider: Arc<Provider<Http>> = Arc::new(provider);  // Wrap the provider in an Arc
-    
+
 
     // Create contract instances
     let exchange_router_contract: EXCHANGE_ROUTER<_> = EXCHANGE_ROUTER::new(exchange_router_address, provider.clone());
     let usdc_native_contract: USDC_NATIVE<_> = USDC_NATIVE::new(usdc_native_address, provider.clone());
     let vault_contract: VAULT<_> = VAULT::new(vault_address, provider.clone());
 
-    let deposit_vault_address = "deposit_vault_address_here";
-    let usdc_address = "usdc_contract_address_here";
-    let wnt_amount = "amount_in_wei_here";
-    let usdc_amount = "amount_in_wei_here";
-    let deposit_params = "/* create your deposit params here */";
+    // Example vars
+    // TODO: make these arguments passed to call builder func.
+    let wnt_amount: &str = "amount_in_wei_here";
+    let usdc_amount: &str = "10000000";
+    let deposit_params: &str = "/* create your deposit params here */";
 
-    let tx = usdc_native_contract
-    .method::<_, ()>("approve", (router_address, approve_amount))?
+
+
+    // ---------------------------------------------------------
+    //                    Multicall Builder
+    // --------------------------------------------------------- 
+
+    let approve_tx = usdc_native_contract
+    .method::<_, ()>("approve", (router_address, usdc_amount))?
     .send()
     .await?;
+
+    let send_wnt_data = exchange_router_contract
+        .encode_function_data("sendWnt", (deposit_vault_address, wnt_amount))?;
+
 
     let function_name: &str = "multicall";
     let function_params = ();
