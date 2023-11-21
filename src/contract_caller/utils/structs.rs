b192db08163b3cef464aa1b813e1c5f314aaff44
50 <jonofeasby4@gmail.com>
2023-11-21 15:12:41 +0000

    add token addresses

--- src/contract_caller/utils/structs.rs
+++ src/contract_caller/utils/structs.rs
@@ -98,19 +98,19 @@ impl Token {
     pub fn info(&self) -> TokenInfo {
         match self {
             Token::ETH => TokenInfo { name: "ETH", address: "0x...EthAddress...", decimals: 18 },
-            Token::WETH => TokenInfo { name: "WETH", address: "0x...WEthAddress...", decimals: 18 },
-            Token::BTC => TokenInfo { name: "BTC", address: "0x...BtcAddress...", decimals: 8 },
-            Token::LINK => TokenInfo { name: "LINK", address: "0x...LinkAddress...", decimals: 18 },
-            Token::ARB => TokenInfo { name: "ARB", address: "0x...ArbAddress...", decimals: 18 },
+            Token::WETH => TokenInfo { name: "WETH", address: "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1", decimals: 18 },
+            Token::BTC => TokenInfo { name: "BTC", address: "0x2f2a2543B76A4166549F7aaB2e75Bef0aefC5B0f", decimals: 8 },
+            Token::LINK => TokenInfo { name: "LINK", address: "0xf97f4df75117a78c1A5a0DBb814Af92458539FB4", decimals: 18 },
+            Token::ARB => TokenInfo { name: "ARB", address: "0x912CE59144191C1204E64559FE8253a0e49E6548", decimals: 18 },
             Token::SOL => TokenInfo { name: "SOL", address: "0x...SolAddress...", decimals: 9 },
-            Token::UNI => TokenInfo { name: "UNI", address: "0x...UniAddress...", decimals: 18 },
+            Token::UNI => TokenInfo { name: "UNI", address: "0xFa7F8980b0f1E64A2062791cc3b0871572f1F7f0", decimals: 18 },
             Token::XRP => TokenInfo { name: "XRP", address: "0x...XrpAddress...", decimals: 6 },
             Token::DOGE => TokenInfo { name: "DOGE", address: "0x...DogeAddress...", decimals: 8 },
             Token::LTC => TokenInfo { name: "LTC", address: "0x...LtcAddress...", decimals: 8 },
-            Token::USDC => TokenInfo { name: "USDC", address: "0x...USDCAddress...", decimals: 18 },
-            Token::USDCE => TokenInfo { name: "USDCE", address: "0x...USDCEAddress...", decimals: 6 },
-            Token::DAI => TokenInfo { name: "DAI", address: "0x...DAIAddress...", decimals: 8 },
-            Token::USDT => TokenInfo { name: "USDT", address: "0x...USDTAddress...", decimals: 8 },
+            Token::USDC => TokenInfo { name: "USDC", address: "0xaf88d065e77c8cC2239327C5EDb3A432268e5831", decimals: 6 },
+            Token::USDCE => TokenInfo { name: "USDCE", address: "0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8", decimals: 6 },
+            Token::DAI => TokenInfo { name: "DAI", address: "0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1", decimals: 18 },
+            Token::USDT => TokenInfo { name: "USDT", address: "0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9", decimals: 6 },
         }
     }
 
