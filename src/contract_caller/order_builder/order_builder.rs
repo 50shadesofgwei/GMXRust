6d9229c9f80d517397f76d28d4a79e9aa5d26d72
50 <jonofeasby4@gmail.com>
2023-11-23 16:15:09 +0000

    WIP: commit progress

--- src/contract_caller/order_builder/order_builder.rs
+++ src/contract_caller/order_builder/order_builder.rs
@@ -6,6 +6,12 @@ use crate::contract_caller::order_builder::get_price::fetch_token_price;
 pub async fn calculate_order_params(input: OrderCalcInput) -> Result<OrderCalcOutput, Box<dyn std::error::Error>> {
 
     let mut price_output: TokenPriceFromApiResponse;
+    let mut initial_collateral_delta_amount: U256 = U256::from(0);
+    let mut trigger_price: U256 = U256::from(0);
+    let mut execution_fee: U256 = U256::from(0);
+    let mut min_output_amount: U256 = U256::from(0);
+
+
 
     // Fetch the decimals for the collateral/index tokens
     let collateral_info: TokenInfo = Token::from_name(&input.collateral_token)
@@ -34,16 +40,24 @@ pub async fn calculate_order_params(input: OrderCalcInput) -> Result<OrderCalcOu
     let price_output: TokenPriceFromApiResponse = fetch_token_price(input.index_token.clone()).await?;
 
     let min_price_raw: U256 = U256::from_dec_str(&price_output.min_price_full)?;
-    let min_price: U256 = min_price_raw.checked_mul(U256::exp10(index_info.decimals as usize))
+    let acceptable_price: U256 = min_price_raw.checked_mul(U256::exp10(index_info.decimals as usize))
         .ok_or("Decimal adjustment error for min price")?;
 
     // Apply leverage factor and adjust for USD amount
     let leverage_as_u256: U256 = U256::from(input.leverage_factor as u64);
     let size_usd_raw: U256 = collateral_amount * leverage_as_u256;
-    let size_usd: U256 = size_usd_raw.checked_mul(U256::exp10(30))
+    let size_delta_usd: U256 = size_usd_raw.checked_mul(U256::exp10(30))
         .ok_or("Decimal adjustment error for size in USD")?;
 
 
     // Create and return the OrderObject with calculated values
-    Ok()
+    Ok(OrderCalcOutput {
+        collateral_amount, 
+        size_delta_usd,
+        initial_collateral_delta_amount,
+        trigger_price,
+        acceptable_price,
+        execution_fee,
+        min_output_amount
+    })
 }
