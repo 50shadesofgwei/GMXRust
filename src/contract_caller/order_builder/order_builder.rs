85fca7ae3354ff235773b673e1b398890a4602ac
50 <jonofeasby4@gmail.com>
2023-11-24 11:15:46 +0000

    fix scaling errors

--- src/contract_caller/order_builder/order_builder.rs
+++ src/contract_caller/order_builder/order_builder.rs
@@ -3,7 +3,7 @@ use ethers::types::U256;
 use crate::contract_caller::order_builder::get_price::fetch_token_price;
 use crate::contract_caller::utils::gas_calculator::calculate_execution_fee;
 
-pub async fn calculate_order_params(input: OrderCalcInput) -> Result<OrderCalcOutput, Box<dyn std::error::Error>> {
+pub async fn calculate_market_increase_order_params(input: OrderCalcInput) -> Result<OrderCalcOutput, Box<dyn std::error::Error>> {
     const USD_SCALE_FACTOR: u32 = 30; // GMX's scaling factor for USD values
 
     println!("Starting order parameter calculations...");
@@ -19,35 +19,30 @@ pub async fn calculate_order_params(input: OrderCalcInput) -> Result<OrderCalcOu
         .ok_or("Unsupported token")?
         .info();
 
-    let index_info: TokenInfo = Token::from_name(&input.index_token)
-        .ok_or("Unsupported token")?
-        .info();
-
     println!("Converting and adjusting collateral amount...");
     let collateral_amount_raw: U256 = U256::from_dec_str(&input.collateral_amount)?;
-    let collateral_amount = collateral_amount_raw.checked_mul(U256::exp10(collateral_info.decimals as usize))
-        .ok_or("Decimal adjustment error for collateral amount")?;
+    let actual_usd_value: U256 = collateral_amount_raw.checked_div(U256::exp10(collateral_info.decimals as usize))
+        .ok_or("Decimal adjustment error for actual USD value")?;
 
     println!("Fetching and adjusting prices...");
     let price_output = fetch_token_price(input.index_token.clone()).await?;
 
     println!("Calculating acceptable price...");
-    let min_price_raw: U256 = U256::from_dec_str(&price_output.min_price_full)?;
-    let acceptable_price = min_price_raw.checked_mul(U256::exp10(index_info.decimals as usize))
-        .ok_or("Decimal adjustment error for min price")?;
+    let acceptable_price: U256 = U256::from_dec_str(&price_output.min_price_full)?;
 
     println!("Applying leverage factor and adjusting for USD amount...");
     let leverage_as_u256: U256 = U256::from(input.leverage_factor as u64);
-    let size_usd_raw = collateral_amount * leverage_as_u256;
-    let size_delta_usd = size_usd_raw.checked_mul(U256::exp10(USD_SCALE_FACTOR as usize))
-        .ok_or("Decimal adjustment error for size in USD")?;
+    let leveraged_usd_value: U256 = actual_usd_value.checked_mul(leverage_as_u256)
+        .ok_or("Leverage application error")?;
+    let size_delta_usd: U256 = leveraged_usd_value.checked_mul(U256::exp10(USD_SCALE_FACTOR as usize))
+        .ok_or("Final USD scaling error")?;
 
     println!("Calculating execution fee...");
     let execution_fee = calculate_execution_fee(estimated_gas).await?;
 
     println!("Returning calculated order parameters...");
     Ok(OrderCalcOutput {
-        collateral_amount, 
+        collateral_amount: collateral_amount_raw, 
         size_delta_usd,
         initial_collateral_delta_amount,
         trigger_price,
@@ -56,5 +51,3 @@ pub async fn calculate_order_params(input: OrderCalcInput) -> Result<OrderCalcOu
         min_output_amount
     })
 }
-
-
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 pub mod contract_caller;
 
 use crate::contract_caller::utils::structs::{OrderCalcInput, OrderCalcOutput};
-use crate::contract_caller::order_builder::order_builder::calculate_order_params;
+use crate::contract_caller::order_builder::order_builder::calculate_market_increase_order_params;
 use dotenv::dotenv;
 
 #[tokio::main]
@@ -11,12 +11,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let test: OrderCalcInput = OrderCalcInput {
         order_type: 2,
         collateral_token: "USDC".to_string(),
-        collateral_amount: "10000000000".to_string(),
+        collateral_amount: "1000000000".to_string(),
         index_token: "ETH".to_string(),
         leverage_factor: 10.0,
     };
 
-    let test_result: OrderCalcOutput = calculate_order_params(test).await?;
+    let test_result: OrderCalcOutput = calculate_market_increase_order_params(test).await?;
 
     // Use test_result for something, or print it out
     println!("{:?}", test_result);
